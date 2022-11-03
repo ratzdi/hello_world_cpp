7aec18ab1fb5c7604bb215a98a904c2c14cab1ae
Dimitri Ratz <dimitri.ratz@pm.me>
2022-11-03 10:53:58 +0100

    Remove some bug.

--- src/library.cpp
+++ src/library.cpp
@@ -8,7 +8,7 @@ void HelloWorld::print_hello() const
     std::cout << "Hello World from Hello World Library!" << std::endl;
 }
 
-std::string HelloWorld::say_hello() noexcept
+std::string HelloWorld::say_hello() const noexcept
 {
     return std::string{"Hello World!"};
 }
